8967aafcca8cee70ba11bb74399a1d6b69a2610b
Ravi Kumar <bhardwajravi2025@gmail.com>
2025-11-23 23:03:58 +0530

    min stack

---
+++ SearchinAndSorting/findMinimumInSortedRotatedArray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size()-1;
+        int mid = start + (end-start)/2;
+
+        while(start < end){
+           if(nums[mid] > nums[end]) start = mid+1;
+           else if(nums[mid] < nums[end]) end = mid;
+           else end--;
+           mid = start + (end-start)/2;
+        }
+        return nums[start];
+    }
+};
