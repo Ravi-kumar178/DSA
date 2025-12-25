b294578baff8e0a4827c2e0b1c6fc63a41bea4f8
Ravi Kumar <bhardwajravi2025@gmail.com>
2025-12-25 13:07:34 +0530

    pathsum3

---
+++ Tree/PathSum3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int ans = 0;
+    void pathFromOneRoot(TreeNode* root, long long sum){
+        if(!root) return;
+
+        if(sum == root->val){
+            ans = ans+1;
+        }
+
+        pathFromOneRoot(root->left,sum-root->val);
+        pathFromOneRoot(root->right,sum-root->val);
+    };
+
+    int pathSum(TreeNode* root, int targetSum) {
+        if(root){
+            pathFromOneRoot(root,targetSum);
+            pathSum(root->left,targetSum);
+            pathSum(root->right,targetSum);
+        }
+        return ans;
+    }
+};
